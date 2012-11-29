e4d934d30067ab87059eb5a6f4008fa68b2c23c7
Vaclav Pacholik <zahuba@seznam.cz>
2012-11-29 20:24:20 +0100

    ddd

--- src/PGR-projekt/Scene.cpp
+++ src/PGR-projekt/Scene.cpp
@@ -32,36 +32,30 @@ void Scene::Init()
     primitives.back()->SetMaterial(Material(glm::vec4(1, 1, 0, 1), 0.f));
     primitives.back()->SetLigth(true);*/
 
-    //ground
+
     primitives.push_back(new Plane(glm::vec3(0, 1, 0), 4.4f));
     primitives.back()->SetMaterial(Material(glm::vec4(.4f, .3f, .3f, 1), 1.f));
 
-    //big sphere
     primitives.push_back(new Sphere(glm::vec3(1, -.8f, 3), 2.5f));
-    primitives.back()->SetMaterial(Material(glm::vec4(.7f, .7f, .7f, 1), 0.2f, 0.6f));
-    
-    //small sphere
-    primitives.push_back(new Sphere(glm::vec3(-5.5f, -.5f, 7), 2.0f));
-    primitives.back()->SetMaterial(Material(glm::vec4(.7f, .7f, 1.f, 1), 0.1f, 1.f));
+    primitives.back()->SetMaterial(Material(glm::vec4(.7f, .7f, .7f, 1)));
 
-    //light 1
-    primitives.push_back(new Sphere(glm::vec3(0, 5, 5), 0.1f));
-    primitives.back()->SetMaterial(Material(glm::vec4(.4f, .4f, .4f, 1)));
+    //primitives.push_back(new Sphere(glm::vec3(-5.5f, -.5f, 7), 2.0f));
+    //primitives.back()->SetMaterial(Material(glm::vec4(.7f, .7f, 1.f, 1), 0.1f));
+
+    primitives.push_back(new Sphere(glm::vec3(-8, 5, 15), 0.4f));
+    primitives.back()->SetMaterial(Material(glm::vec4(.6f, .6f, .6f, 1)));
     primitives.back()->SetLigth(true);
 
-    //light 2
     primitives.push_back(new Sphere(glm::vec3(2, 5, 1), 0.1f));
-    primitives.back()->SetMaterial(Material(glm::vec4(.6f, .6f, .8f, 1)));
+    primitives.back()->SetMaterial(Material(glm::vec4(.7f, .7f, .9f, 1)));
     primitives.back()->SetLigth(true);
 
 
-    //primitives.push_back(new Sphere(glm::vec3(0, 0, -1), 0.1f));
-    //primitives.back()->SetMaterial(Material(glm::vec4(.9f, .9f, .9f, 1), 10));
-    //primitives.back()->SetLigth(true);
+   /* primitives.push_back(new Sphere(glm::vec3(0, 0, -1), 0.1f));
+    primitives.back()->SetMaterial(Material(glm::vec4(.9f, .9f, .9f, 1), 10));
+    primitives.back()->SetLigth(true);*/
 
-    //primitives.push_back(new Particle(glm::vec3(-5.5f, 1.5f, 7), glm::vec3(0, 0, 1), 2.0f));
-    //primitives.back()->SetMaterial(Material(glm::vec4(.2f, 1.f, .2f, 1), .6f));
-
-
+    primitives.push_back(new Particle(glm::vec3(-5.5f, 1.5f, 7), glm::vec3(0, 0, 1), 2.0f));
+    primitives.back()->SetMaterial(Material(glm::vec4(.2f, 1.f, .2f, 0.6f), .2f, .0f , 1.5f, 1.0f));
 
 }
