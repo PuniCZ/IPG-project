600a87d5169d4b14dbc177e4a08ba634d92374bc
lukes <lukaspijak@gmail.com>
2012-12-05 22:33:20 +0100

    Uprava ovladani
    Signed-off-by: lukes <lukaspijak@gmail.com>

--- src/PGR-projekt/main.cpp
+++ src/PGR-projekt/main.cpp
@@ -54,7 +54,7 @@ int main (int /*argc*/, char ** /*argv*/)
     Scene scene = Scene();
     scene.SetFogFactor(0.0f);
 
-    scene.Init(10);
+    scene.Init(0);
 
     Raytracer raytracer = Raytracer(&camera, scene);
     
@@ -83,25 +83,35 @@ int main (int /*argc*/, char ** /*argv*/)
                         renderFinished = false;
                         break;
 					case SDLK_1:
+						printf("1-Mrak\n");
                         scene.Init(1);
                         raytracer.Init();
                         renderFinished = false;
+						break;
 					case SDLK_2:
+						printf("5-Mraku\n");
                         scene.Init(5);
                         raytracer.Init();
                         renderFinished = false;
+						break;
 					case SDLK_3:
+						printf("10-Mraku\n");
                         scene.Init(10);
                         raytracer.Init();
                         renderFinished = false;
+						break;
 					case SDLK_4:
-                        scene.Init(15);
-                        raytracer.Init();
-                        renderFinished = false;
-					case SDLK_5:
+						printf("20-Mraku\n");
                         scene.Init(20);
                         raytracer.Init();
                         renderFinished = false;
+						break;
+					case SDLK_5:
+						printf("100-Mraku\n");
+                        scene.Init(100);
+                        raytracer.Init();
+                        renderFinished = false;
+						break;
                 }
             }
         }
